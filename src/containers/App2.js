82536acb9fdeffbb8311b1bff81bbeda4ea3c09f
mz-suzin <mzsuzin@gmail.com>
2022-08-18 22:12:01 -0300

    updated the code to use hooks instead of class

--- src/containers/App2.js
+++ src/containers/App2.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 //import { robots } from './robots'; //i'm deestructuring this one because the export method is not default. It is exporting a const variable.
 import SearchBox from '../components/SearchBox';
@@ -6,38 +6,33 @@ import './App2.css'
 import Scroll from '../components/Scroll'
 import ErrorBoundary from '../components/ErrorBoundary';
 
-class App2 extends React.Component { //this is a smart component
-    constructor() {
-        super(); 
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
+function App2 () {
+    const [robots, setRobots] = useState([]); //this is array destructuring
+    const [searchfield, setSearchfield] = useState('');
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => setRobots(users));
+    }, []); //the empty array here means that I want this useEffect function only once.
+    //it actually means that useEffect will only run when this empty array changes, which means never.
+    //If I put [robots] in there instead, it will create an infinite loop, because the function is using setRobots
+    //which updates robots, which calls useEffects, which updates robots, which calls useEffects...
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value) 
     }
 
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users') //we fetch whatever there is in this url
-            .then(response => response.json()) //json magic here I don't understand
-            .then(users => this.setState({robots: users})); //update the object
-    }
-
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value }) //always use setState() to change the state of an object
-    }
-
-    render() {
-        const { robots, searchfield } = this.state; //just to make it cleaner
-
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-        })
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
 
         return !robots.length ? //if
         <h1>Loading</h1> : //true
         ( //false
             <div className='tc'>
                 <h1>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
+                <SearchBox searchChange={onSearchChange}/>
                 <Scroll>
                     <ErrorBoundary>
                         <CardList robots={filteredRobots}/>
@@ -46,7 +41,49 @@ class App2 extends React.Component { //this is a smart component
                 {/* <footer>Designed with many doubts - Iorek INC</footer> */}
             </div>
         );
-    }
 }
+
+// class App2 extends React.Component { //this is a smart component
+//     constructor() {
+//         super(); 
+//         this.state = {
+//             robots: [],
+//             searchfield: ''
+//         }
+//     }
+
+//     componentDidMount() {
+//         fetch('https://jsonplaceholder.typicode.com/users') //we fetch whatever there is in this url
+//             .then(response => response.json()) //json magic here I don't understand
+//             .then(users => this.setState({robots: users})); //update the object
+//     }
+
+//     onSearchChange = (event) => {
+//         this.setState({ searchfield: event.target.value }) //always use setState() to change the state of an object
+//     }
+
+//     render() {
+//         const { robots, searchfield } = this.state; //just to make it cleaner
+
+//         const filteredRobots = robots.filter(robot => {
+//             return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+//         })
+
+//         return !robots.length ? //if
+//         <h1>Loading</h1> : //true
+//         ( //false
+//             <div className='tc'>
+//                 <h1>RoboFriends</h1>
+//                 <SearchBox searchChange={this.onSearchChange}/>
+//                 <Scroll>
+//                     <ErrorBoundary>
+//                         <CardList robots={filteredRobots}/>
+//                     </ErrorBoundary>
+//                 </Scroll>
+//                 {/* <footer>Designed with many doubts - Iorek INC</footer> */}
+//             </div>
+//         );
+//     }
+// }
 
 export default App2;
